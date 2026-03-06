691eb8e7722da0679fd6a2c77ef28c3ac0e3048e
laska01 <lifejanek@gmail.com>
2026-03-06 11:08:44 +0100

    daozwierze

--- app/src/main/java/com/example/pracanalekcjibazydanych/MainActivity.java
+++ app/src/main/java/com/example/pracanalekcjibazydanych/MainActivity.java
@@ -9,16 +9,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    ZwierzetaDatabase zwierzetaDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+
+        zwierzetaDatabase = ZwierzetaDatabase.ZwrocinstancjeBazydanych(MainActivity.this);
+        zwierzetaDatabase.ZwrocWszystkieZwierzetaZbazy().wstawZwierzeDoBazy(new Zwierzeta("Marcin", "czarny", 67));
+        zwierzetaDatabase.ZwrocWszystkieZwierzetaZbazy().wstawZwierzeDoBazy(new Zwierzeta("Marcin", "czarny", 67));
+        zwierzetaDatabase.ZwrocWszystkieZwierzetaZbazy().wstawZwierzeDoBazy(new Zwierzeta("Marcin", "czarny", 67));
+    };
 }
---
+++ app/src/main/java/com/example/pracanalekcjibazydanych/ZwierzetaDatabase.java
@@ -0,0 +1,27 @@
+package com.example.pracanalekcjibazydanych;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Zwierzeta.class}, version = 1)
+public abstract class ZwierzetaDatabase extends RoomDatabase {
+    public abstract ZwierzetaDao ZwrocWszystkieZwierzetaZbazy();
+
+    private static ZwierzetaDatabase instancje;
+
+    public static ZwierzetaDatabase ZwrocinstancjeBazydanych(Context context){
+        if(instancje == null){
+            instancje = Room.databaseBuilder(
+                    context,
+                    ZwierzetaDatabase.class,
+                        "zwierzeta_db"
+            )
+                    .allowMainThreadQueries()
+                    .build();
+        }
+        return instancje;
+    }
+}
