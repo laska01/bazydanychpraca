5d2e75fd766eb8c8f1a769c2cd0cdfb6dd617b67
laska01 <lifejanek@gmail.com>
2026-03-06 10:17:33 +0100

    klasa z gettersetterIkonstruktor

---
+++ app/src/main/java/com/example/pracanalekcjibazydanych/Zwierzeta.java
@@ -0,0 +1,53 @@
+package com.example.pracanalekcjibazydanych;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "zwiereta_tabela")
+public class Zwierzeta {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String imie;
+    private String kolor;
+
+    public Zwierzeta( String imie, String kolor, int wiek) {
+        id = 0;
+        this.imie = imie;
+        this.kolor = kolor;
+        this.wiek = wiek;
+    }
+
+    public int getWiek() {
+        return wiek;
+    }
+
+    public void setWiek(int wiek) {
+        this.wiek = wiek;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getKolor() {
+        return kolor;
+    }
+
+    public void setKolor(String kolor) {
+        this.kolor = kolor;
+    }
+
+    private int wiek;
+}
